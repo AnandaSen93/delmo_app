f9e967bff291ae34b1390abc71bb6737d6bd7f67
Mac M1 <testuserwg@yopmail.com>
2025-07-23 13:28:21 +0530

    23rd july login screen

--- lib/helper_and_api/textstyle.dart
+++ lib/helper_and_api/textstyle.dart
@@ -13,6 +13,32 @@ const textStyleForTextField = TextStyle(
 );
 
 
+const textStyleForHeading = TextStyle(
+  fontFamily: 'Roboto',
+  fontSize: 20,
+  color: theam_dark_blue_color,
+  fontWeight: FontWeight.w800, // This uses Roboto-Medium or Bold depending on available weights
+  decoration: TextDecoration.none,
+);
+
+const textStyleForSubHeading = TextStyle(
+  fontFamily: 'Roboto',
+  fontSize: 18,
+  color: grey_color,
+  fontWeight: FontWeight.w500, // This uses Roboto-Medium or Bold depending on available weights
+  decoration: TextDecoration.none,
+);
+
+const textStyleForButton = TextStyle(
+  fontFamily: 'Roboto',
+  fontSize: 18,
+  color: white_color,
+  fontWeight: FontWeight.w400, // This uses Roboto-Medium or Bold depending on available weights
+  decoration: TextDecoration.none,
+);
+
+
+
 
 
 
--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,5 +1,10 @@
+import 'package:delmo_app/helper_and_api/colors.dart';
+import 'package:delmo_app/helper_and_api/textstyle.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+import 'package:country_picker/country_picker.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -9,14 +14,193 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final FocusNode _focusNode = FocusNode();
+  String _countryCode = '+91'; // Default
+
+  void _toggleKeyboard() {
+    if (_focusNode.hasFocus) {
+      _focusNode.unfocus(); // Hide keyboard
+    } else {
+      FocusScope.of(context).requestFocus(_focusNode); // Show keyboard
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return PlatformScaffold(
-      body: SafeArea(child: Column(
-        children: [
-          Text("data")
-        ],
-      )),
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () {
+            _toggleKeyboard();
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 350,
+                  width: double.infinity,
+                  color: theam_dark_blue_color,
+                  child: Center(
+                    child: SizedBox(
+                      height: 150,
+                      width: 150,
+                      child: Image.asset(
+                        "assets/images/login_page_icon.png",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50),
+                Container(
+                  width: double.infinity,
+                  child: Text(
+                    "Login",
+                    textAlign: TextAlign.center,
+                    style: textStyleForHeading,
+                  ),
+                ),
+
+                Container(
+                  padding: EdgeInsets.all(10),
+                  width: double.infinity,
+                  child: Text(
+                    "Please enter your mobile number",
+                    textAlign: TextAlign.center,
+                    style: textStyleForSubHeading.copyWith(
+                      color: Colors.black54,
+                    ),
+                    maxLines: 2,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(25),
+                  child: Container(
+                    height: 50,
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: theam_dark_blue_color, // Black border color
+                        width: 1.0, // Border width
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ), // Optional: Rounded corners
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset("assets/images/mobile.png"),
+                        Container(
+                          height: double.infinity,
+                          //width: 80,
+                         // color: Colors.amberAccent,
+                          child:TextButton(
+                              onPressed: () {
+                                showCountryPicker(
+                                  context: context,
+                                  showPhoneCode: true,
+                                  onSelect: (Country country) {
+                                    setState(() {
+                                      _countryCode = '+${country.phoneCode}';
+                                    });
+                                  },
+                                );
+                              },
+                              child: Row(
+                                children: [
+                                  Text(
+                                    _countryCode,
+                                    style: textStyleForTextField.copyWith(
+                                      color: Colors.black54,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Icon(Icons.arrow_drop_down)
+                                ],
+                              ),
+                            ),
+
+                            // Text("+91",
+                            // style: textStyleForTextField,
+                            // textAlign: TextAlign.center,
+                            // ),
+                        ),
+                        Expanded(
+                          child: PlatformTextField(
+                            textInputAction: TextInputAction.done,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                            ],
+                            //controller: addeditAddressViewModel .postCodeController, // (Optional: TextEditingController, currently commented)
+                            // onChanged: addeditAddressViewModel.setPostCode, // Called on text change
+                            keyboardType: TextInputType.phone,
+                            // minLines: 4,         // Minimum height (optional)
+                            // maxLines: null, // Keyboard optimized for name input
+                            onSubmitted: (_) => _toggleKeyboard(),
+                            hintText:
+                                'Mobile Number', // Common hint text (can override per platform)
+                            // iOS Specific Customization
+                            cupertino: (context, platform) =>
+                                CupertinoTextFieldData(
+                                  decoration: BoxDecoration(
+                                    color: Colors
+                                        .transparent, // No background color for iOS field
+                                  ),
+                                  style: textStyleForTextField,
+                                  // Text color on iOS
+                                ),
+
+                            // Android Specific Customization
+                            material: (context, platform) => MaterialTextFieldData(
+                              style: textStyleForTextField,
+                              decoration: InputDecoration(
+                                filled: false, // No fill color
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.all(
+                                  10,
+                                ), // Shows border around text field
+                                hintText:
+                                    'Mobile Nummber', // Hint text (optional, also passed above)
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 80),
+
+                Padding(
+                  padding: EdgeInsets.all(25),
+                  child: Container(
+                    width: double.infinity,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          theam_sky_blue_color, // Start color (blue)
+                          theam_dark_blue_color, // End color (light blue)
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ), // Optional: Rounded corners
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text("Send OTP → ", style: textStyleForButton),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
